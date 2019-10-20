6aa061334fdd0adc599fae789c1dd11a56c21fee
xu_yh <xu_yh@suixingpay.com>
2019-10-20 22:47:34 +0800

    优化

--- Java se/day27_反射&装饰-模板设计模式&JDK新特性/day27总结-反射和新特性.java
+++ Java se/day27_反射&装饰-模板设计模式&JDK新特性/day27总结-反射和新特性.java
@@ -52,6 +52,7 @@
 		// 我们要创建一个动态代理对象
 		// Proxy类中有一个方法可以创建动态代理对象
 		// public static Object newProxyInstance(ClassLoader loader,Class<?>[] interfaces,InvocationHandler h)
+		// InvocationHandler h，动态处理器
 		// 我准备对ud对象做一个代理对象
 		UserDao ud = new UserDaoImpl();
 		//MyInvocationHandler类实现InvocationHandler接口
